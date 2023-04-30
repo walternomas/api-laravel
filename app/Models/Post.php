ac02c19dfd02b9228eb2d5cf9c663a34bec5469c
walternomas <w.barriosvazquez@gmail.com>
2023-04-30 16:30:20 -0300

    mejorando el aspecto con Tailwind CSS

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function getExcerptAttribute()
+    {
+        return substr($this->content, 0, 120);
+    }
+
+    public function getPublishedAtAttribute()
+    {
+        // return $this->created_at->format('d/m/Y');
+        return $this->created_at->diffForHumans();
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,15 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>API Laravel 10</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    @foreach ($posts as $post)
-        <div>
-            <h2>{{ $post->title }}</h2>
+<body class="bg-gray-100 text-gray-700">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-3 my-10">
+            @foreach ($posts as $post)
+                <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                    <p class="text-xs">{{ $post->excerpt }}</p>
+                    <p class="text-xs text-right">{{ $post->published_at }}</p>
+                </div>
+            @endforeach
         </div>
-    @endforeach
-    {{ $posts->links() }}
+        <div class="mb-10">
+            {{ $posts->links() }}
+        </div>
 </body>
 
 </html>
