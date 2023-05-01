d43de973fc084ae0f11e1b54c7e0cc2e8453ef17
walternomas <w.barriosvazquez@gmail.com>
2023-05-01 12:51:21 -0300

    versión 1: colección

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('v1/posts', \App\Http\Controllers\Api\V1\PostController::class)->only('show');
+Route::apiResource('v1/posts', \App\Http\Controllers\Api\V1\PostController::class)
+    ->only(['index', 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
